45d4e163face28493624d366bf4be88c8256816a
Kyle Richey <kylerichey@users.noreply.github.com>
2016-03-30 16:24:05 +0000

    merged kyle Matt code

--- private/javascripts/coreController.js
+++ private/javascripts/coreController.js
@@ -84,12 +84,13 @@ function($scope,$http){
 		return $scope.playerName;
 	};	
 
-	// TODO : Get Body Level name from database
-	// $scope.playerBodyLevel = "Far Below Average";	
 	
 	$scope.getPlayerBodyLevel = function(){
 		return $scope.playerBodyLevel;
 	};
+	
+	//growth function
+	$scope.globalCostGrowthRate = 0.05;
 	
 	/* ******************************************
 	******** * Base Currencies	*****************
@@ -100,7 +101,6 @@ function($scope,$http){
 	******** *Penny		************************
 	*********************************************/	
 	$scope.pennyCount = 1000;
-	$scope.pennyTooltip = "You use penny to buy things";
 	$scope.pennyRate = 0;
 	$scope.pennyUnlocked = true;
 
@@ -123,16 +123,19 @@ function($scope,$http){
 	/* ******************************************
 	******** *Pencil   ************************
 	*********************************************/	
-	$scope.pencilCount = 0;
-	$scope.pencilTooltip ="It's often good to write stuff down. Cost :10 penny";
+	$scope.pencilCount = 0;	
 	$scope.pencilRate = 0;
 	$scope.pencilUnlocked = false;
+	$scope.pencilCost = 10;
 	
-	$scope.increasePencilCount = function() {
-		if($scope.pennyCount>=10){					
-			$scope.pennyCount = $scope.pennyCount -10;
+	$scope.increasePencilCount = function() {				
+		if($scope.pennyCount>= $scope.pencilCost){					
+			$scope.pennyCount = $scope.pennyCount - $scope.pencilCost;
 			$scope.pencilCount = $scope.pencilCount + 1;
-			$scope.bookUnlocked =true;
+			$scope.pencilCost = Math.round($scope.pencilCost + ($scope.globalCostGrowthRate * $scope.pencilCost));  //calculate new cost			
+			if(!$scope.bookUnlocked){
+				$scope.bookUnlocked =true;
+			}
 		}
 		else{		
 			$scope.addEntryToConsole("Not enough penny to buy pencil");				 
@@ -147,18 +150,24 @@ function($scope,$http){
 		return Math.round($scope.pencilRate *1000)/100;	
 	};	
 	
+	$scope.getPencilCost = function (){		
+		return Math.round($scope.pencilCost);	
+	};		
+
+	
 	/* ******************************************
 	******** *Book		************************
 	*********************************************/			
-	$scope.bookCount= 0;	
-	$scope.bookTooltip = "Overpriced and can't find a pdf online. Cost: 20 penny";
+	$scope.bookCount= 0;		
 	$scope.bookRate = 0;
 	$scope.bookUnlocked = false;
+	$scope.bookCost = 20;
 	
-	$scope.increaseBookCount = function() {
-	if($scope.pennyCount>=20){					
-		$scope.pennyCount = $scope.pennyCount - 20;
+	$scope.increaseBookCount = function() {	
+	if($scope.pennyCount>=$scope.bookCost){					
+		$scope.pennyCount = $scope.pennyCount - $scope.bookCost;
 		$scope.bookCount = $scope.bookCount + 1;
+		$scope.bookCost = Math.round($scope.bookCost + ($scope.globalCostGrowthRate * $scope.bookCost));
 		if(!$scope.classTabUnlocked){
 			$scope.classTabUnlocked = true;
 			$scope.addEntryToConsole("Class Tab Unlocked");				 
@@ -175,6 +184,13 @@ function($scope,$http){
 	$scope.getBookRate = function (){
 		return Math.round($scope.bookRate *1000)/100;	
 	};
+	
+	$scope.getBookCost = function () {
+		return Math.round($scope.bookCost);
+	}	
+
+	
+
 	
 	/* ******************************************
 	******** *Knowledge		************************
@@ -201,18 +217,19 @@ function($scope,$http){
 	/* ******************************************
 	******** *English 101************************
 	*********************************************/	
-	$scope.eng101Count = 0;
-	$scope.eng101Tooltip = "The first class of any respectable college student. Cost: 3 book";
-	$scope.eng101StudyTooltip = "As if I didn't already know enligsh.\nCost: 2 Pencil.\nReward: 1 knowledge";
-	$scope.eng101FinalTooltip = "Do or do not, there is no try. Wait, thats not Shakespeare. Cost: 10 Pencil. Reward: 10 Knowledge";
+	$scope.eng101Count = 0;	
 	$scope.eng101Unlocked = true;
+	$scope.eng101Cost = 3;
+	$scope.eng101StudyCost = 2;
+	$scope.eng101FinalCost = 10;
 	
-	$scope.classEng101 = function() { 
-		if($scope.bookCount>=3){
-			$scope.bookCount = $scope.bookCount - 3;
+	$scope.classEng101 = function() { 	
+		if($scope.bookCount>=$scope.eng101Cost){
+			$scope.bookCount = $scope.bookCount - $scope.eng101Cost;
 			$scope.knowledgeRate = $scope.knowledgeRate + 0.006;
 			$scope.eng101Count = $scope.eng101Count + 1;
 			$scope.knowledgeUnlocked = true;
+			$scope.eng101Cost = Math.round($scope.eng101Cost + ($scope.globalCostGrowthRate * $scope.eng101Cost));
 				if($scope.jobsTabUnlocked == false){
 					$scope.jobsTabUnlocked = true;
 					$scope.addEntryToConsole("Jobs Tab Unlocked");	
@@ -222,11 +239,12 @@ function($scope,$http){
 			}
 	};
 	
-	$scope.classEng101Study = function(){
+	$scope.classEng101Study = function(){		
 		if($scope.eng101Count>0){
-			if($scope.pencilCount>=2){			 
-				$scope.pencilCount = $scope.pencilCount -2;
+			if($scope.pencilCount>=$scope.eng101StudyCost){			 
+				$scope.pencilCount = $scope.pencilCount -$scope.eng101StudyCost;
 				$scope.knowledgeCount = $scope.knowledgeCount +1;
+				$scope.eng101StudyCost = Math.round($scope.eng101StudyCost + ($scope.globalCostGrowthRate * $scope.eng101StudyCost));
 			} else{
 				$scope.addEntryToConsole("Not enough pencil to study");
 			}
@@ -236,7 +254,7 @@ function($scope,$http){
 	};
 	
 	$scope.eng101CanStudy = function(){
-		if($scope.eng101Count>0 && $scope.pencilCount>=2 ){
+		if($scope.eng101Count>0 && $scope.pencilCount>= $scope.eng101StudyCost ){
 			return true;
 		}else{
 			return false;
@@ -244,10 +262,11 @@ function($scope,$http){
 	};	
 	$scope.classEng101Final = function(){
 		if($scope.eng101Count>0){
-			if($scope.pencilCount>=10){			 
-				$scope.pencilCount = $scope.pencilCount -10;
+			if($scope.pencilCount>=$scope.eng101FinalCost){			 
+				$scope.pencilCount = $scope.pencilCount -$scope.eng101FinalCost;
 				$scope.knowledgeCount = $scope.knowledgeCount +10;
 				$scope.cs142Unlocked = true;
+				$scope.eng101FinalCost = Math.round($scope.eng101FinalCost + ($scope.globalCostGrowthRate * $scope.eng101FinalCost));
 			} else{
 				$scope.addEntryToConsole("Not enough pencil to take final");
 			}
@@ -257,7 +276,7 @@ function($scope,$http){
 	};
 	
 	$scope.eng101CanFinal = function(){
-		if($scope.eng101Count>0 && $scope.pencilCount>=10 ){
+		if($scope.eng101Count>0 && $scope.pencilCount>=$scope.eng101FinalCost ){
 			return true;
 		} else {
 		return false;
@@ -268,20 +287,39 @@ function($scope,$http){
 		return $scope.eng101Count;
 	};
 	
+	
+	$scope.getEng101Cost = function (){
+		return Math.round($scope.eng101Cost );
+	}
+		
+	$scope.getEng101StudyCost = function (){
+		return Math.round($scope.eng101StudyCost );
+	}	
+	
+	$scope.getEng101FinalCost = function (){
+		return Math.round($scope.eng101FinalCost );
+	}	
+
+	
 	/* ******************************************
 	******** *CS 142************************
 	*********************************************/		
 	$scope.cs142Unlocked = false;
 	$scope.cs142Count = 0;
+	$scope.cs142Cost = 10;
+	$scope.cs142StudyCost = 5;
+	$scope.cs142FinalCost =15;
+	
 	$scope.cs142Tooltip = "The second most failed class in college. Cost: 10 Books";
 	$scope.cs142StudyTooltip ="Or trade it to your friend for magic cards. Cost: 5 pencil. Reward: 10 Knowledge";
 	$scope.cs142FinalTooltip = "Me.hasStudied? A+, F. I think that's right? Cost: 15 Pencil. Reward: 30 Knowledge";
 	
 	$scope.classCS142 = function() { 
-		if($scope.bookCount>=10){
-			$scope.bookCount = $scope.bookCount - 10;
+		if($scope.bookCount>= $scope.cs142Cost){
+			$scope.bookCount = $scope.bookCount - $scope.cs142Cost;
 			$scope.knowledgeRate = $scope.knowledgeRate + 0.012;
-			$scope.cs142Count = $scope.cs142Count + 1;							
+			$scope.cs142Count = $scope.cs142Count + 1;	
+			$scope.cs142Cost = Math.round($scope.cs142Cost + ($scope.globalCostGrowthRate * $scope.cs142Cost));			
 		}else {
 			$scope.addEntryToConsole("Not enough book to take CS 142");				
 			}
@@ -289,9 +327,10 @@ function($scope,$http){
 	
 	$scope.classCS142Study = function(){
 		if($scope.cs142Count>0){
-			if($scope.pencilCount>=5){			 
-				$scope.pencilCount = $scope.pencilCount -5;
+			if($scope.pencilCount>= $scope.cs142StudyCost){			 
+				$scope.pencilCount = $scope.pencilCount -$scope.cs142StudyCost;
 				$scope.knowledgeCount = $scope.knowledgeCount +10;
+				$scope.cs142StudyCost = Math.round($scope.cs142StudyCost + ($scope.globalCostGrowthRate * $scope.cs142StudyCost));	
 			} else{
 				$scope.addEntryToConsole("Not enough pencil to do lab");
 			}
@@ -301,7 +340,7 @@ function($scope,$http){
 	};
 	
 	$scope.cs142CanStudy = function(){
-		if($scope.cs142Count>0 && $scope.pencilCount>=5 ){
+		if($scope.cs142Count>0 && $scope.pencilCount>= $scope.cs142StudyCost ){
 			return true;
 		}else{
 			return false;
@@ -309,9 +348,10 @@ function($scope,$http){
 	};	
 	$scope.classCS142Final = function(){
 		if($scope.cs142Count>0){
-			if($scope.pencilCount>=15){			 
-				$scope.pencilCount = $scope.pencilCount -15;
+			if($scope.pencilCount>=$scope.cs142FinalCost){			 
+				$scope.pencilCount = $scope.pencilCount - $scope.cs142FinalCost;
 				$scope.knowledgeCount = $scope.knowledgeCount +30;
+				$scope.cs142FinalCost = Math.round($scope.cs142FinalCost + ($scope.globalCostGrowthRate * $scope.cs142FinalCost));	
 				//TODO add next class unlock
 			} else{
 				$scope.addEntryToConsole("Not enough pencil to take final");
@@ -322,7 +362,7 @@ function($scope,$http){
 	};
 	
 	$scope.cs142CanFinal = function(){
-		if($scope.cs142Count>0 && $scope.pencilCount>=15 ){
+		if($scope.cs142Count>0 && $scope.pencilCount>= $scope.cs142FinalCost){
 			return true;
 		} else {
 		return false;
@@ -333,19 +373,36 @@ function($scope,$http){
 		return  Math.round($scope.cs142Count *100)/100;	
 	};
 	
+	$scope.getCS142Cost = function () {
+		return Math.round($scope.cs142Cost);
+	};
+	
+	$scope.getCS142StudyCost = function () {
+		return Math.round($scope.cs142StudyCost);
+	};
+	
+	$scope.getCS142FinalCost = function () {
+		return Math.round($scope.cs142FinalCost);
+	};
+	
 	/* ******************************************
 	******** *Jobs   ************************
 	*********************************************/	
 	$scope.jobsTabUnlocked = false;
+	$scope.isCurrentlyWorking = false;
 	
 	/* ******************************************
 	******** *Burger Flipper  ********************
 	*********************************************/	
 	
+	$scope.mcdonaldsUnlocked = false;
 	$scope.jobBurgerFlipperTooltip = "Pretty much the only job someone as dumb as you could get. Cost: 3 knowledge Reward: 1 penny per hour";
+	
+	$scope.mcdonalds
 	
 	$scope.jobBurgerFlipper = function(){		
 		if($scope.knowledgeCount>=3){
+			$scope.mcdonaldsUnlocked = true;
 			$scope.knowledgeCount = $scope.knowledgeCount - 3;
 			$scope.pennyRate = 	$scope.pennyRate+ 0.006;	
 		} else {
@@ -353,13 +410,55 @@ function($scope,$http){
 			}	
 	};
 	  
+	  $scope.isMcdonaldsUnlocked = function () {
+		  if($scope.mcdonaldsUnlocked)
+		  {
+			 return "Hired"; 
+		  } else {
+			  return " ";
+		  }
+	  };
 
 
+	/* ******************************************
+	******** *Dynamic content filler ********************
+	*********************************************/	
 	
-
+	$scope.dynamicEntryCount = 1;
+	
+	$scope.pencilColorConditions = function() {
+		//$scope.getPennyCount()>= $scope.getPencilCost() ? "'btn-primary'" : "'btn-default'" 
+		if($scope.getPennyCount()>= $scope.getPencilCost())
+		{
+			return "btn-primary"
+		} else{
+			return "btn-default";
+		}
+		
+		
+	};
+	
+	$scope.addEntryToDynamic = function(buttonText){
+		$scope.dynamicEntryCount = $scope.dynamicEntryCount +1;
+		
+		$scope.dynamicList.push({buttonText:buttonText, number:$scope.dynamicEntryCount}); 	
+		};
+		//Default user console value
+		$scope.dynamicList = [
+			{buttonText:'Buy pencil',
+			getCostFunction: $scope.getPencilCost, 
+			toolTip: "Always good to take notes in college.", 
+			colorCondition: $scope.pencilColorConditions,
+			clickCondition: $scope.increasePencilCount,
+			number:0}	
+			
+			//getPennyCount()>=getPencilCost() ? 'btn-primary': 
+		];
+	
 
 	
 	// End of main function
 	} 
 ]);
+
 
